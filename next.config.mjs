45d7ebd1b95592973bd9ff572725a836a1a21c7d
Antonio Pitasi <antonio@pitasi.dev>
2023-03-22 09:14:42 +0100

    add docker-compose for future prod deployments

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,8 @@
 const config = {
   reactStrictMode: true,
 
+  output: "standalone",
+
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
    * must comment the below `i18n` config out.
