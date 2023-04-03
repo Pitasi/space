46db1c84e2d06b5d96efd266badf59cf5f3f6608
Antonio Pitasi <antonio@pitasi.dev>
2023-04-03 19:32:33 +0200

    little fix on desktop

--- src/components/LayoutWithList.tsx
+++ src/components/LayoutWithList.tsx
@@ -19,7 +19,7 @@ export default function LayoutWithList<ItemT>(
           navigation={items.map((i) => itemToPath(i))}
         />
 
-        <article className="flex w-full overflow-scroll rounded-l-3xl bg-fantasy-100 empty:hidden">
+        <article className="flex w-full overflow-scroll rounded-l-3xl bg-fantasy-100">
           {props.children}
         </article>
       </section>
