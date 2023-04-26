e031839e6f7f0eb10a872a47404dded959051fff
Antonio Pitasi <antonio@pitasi.dev>
2023-04-26 19:24:52 +0200

    refactor onPaste logic

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -3,8 +3,41 @@ import { UploadResponse } from "~/app/files/route";
 
 const placeholder = "![Uploading image...]()";
 
-const isPastingImage = (items: DataTransferItemList) =>
-  Array.from(items).some((item) => item.type.startsWith("image/"));
+const isImage = (file: File): boolean => file.type.startsWith("image/");
+
+const upload = async (file: File): Promise<UploadResponse> => {
+  const formdata = new FormData();
+  formdata.append("file", file);
+  const res = await fetch("/files", {
+    method: "POST",
+    body: formdata,
+  });
+  if (!res.ok) {
+    throw new Error("Failed to upload image");
+  }
+
+  const data = (await res.json()) as UploadResponse;
+  return data;
+};
+
+const insertImagePlaceholder = (textarea: HTMLTextAreaElement) => {
+  const start = textarea.selectionStart;
+  textarea.value =
+    textarea.value.slice(0, start) +
+    "\n" +
+    placeholder +
+    "\n" +
+    textarea.value.slice(textarea.selectionEnd);
+  textarea.selectionStart = start + placeholder.length + 2; // +2 for '\n's
+  textarea.selectionEnd = textarea.selectionStart;
+};
+
+const replaceImagePlaceholder = (
+  textarea: HTMLTextAreaElement,
+  path: string
+) => {
+  textarea.value = textarea.value.replace(placeholder, `![image](${path})`);
+};
 
 type EditorTextareaProps = JSX.IntrinsicAttributes &
   ClassAttributes<HTMLTextAreaElement> &
@@ -17,43 +50,21 @@ function _EditorTextarea(
   let isUploading = false;
   const onPaste = async (e: React.ClipboardEvent<HTMLTextAreaElement>) => {
     const items = e.clipboardData.items;
+    const imageFiles = [...items]
+      .flatMap((item) => item.getAsFile() || [])
+      .filter(isImage);
     const textarea = e.currentTarget;
-    if (!isUploading && items.length > 0 && isPastingImage(items)) {
+    if (!isUploading) {
       e.preventDefault();
-
       isUploading = true;
-      const start = textarea.selectionStart;
-      textarea.value =
-        textarea.value.slice(0, start) +
-        "\n" +
-        placeholder +
-        "\n" +
-        textarea.value.slice(textarea.selectionEnd);
-      textarea.selectionStart = start + placeholder.length;
-      textarea.selectionEnd = textarea.selectionStart;
 
-      const item = items[0] as DataTransferItem;
-      const file = item.getAsFile();
-      if (file) {
-        const formdata = new FormData();
-        formdata.append("file", file);
-        const res = await fetch("/files", {
-          method: "POST",
-          body: formdata,
-        });
-        if (!res.ok) {
-          throw new Error("Failed to upload image");
-        }
-
-        const data = (await res.json()) as UploadResponse;
-
-        textarea.value = textarea.value.replace(
-          placeholder,
-          `![image](${data.path})`
-        );
-
-        isUploading = false;
+      for (const file of imageFiles) {
+        insertImagePlaceholder(textarea);
+        const { path } = await upload(file);
+        replaceImagePlaceholder(textarea, path);
       }
+
+      isUploading = false;
     }
   };
   return <textarea ref={ref} onPaste={onPaste} {...props} />;
