650fa83877e0398d3b06eb85b625b117c6a08651
Antonio Pitasi <antonio@pitasi.dev>
2023-05-01 15:05:10 +0200

    fix blockquote element

--- src/components/ui/typography/TypographyBlockquote.tsx
+++ src/components/ui/typography/TypographyBlockquote.tsx
@@ -1,8 +1,5 @@
-export function TypographyBlockquote() {
-  return (
-    <blockquote className="mt-6 border-l-2 border-slate-300 pl-6 italic text-slate-800 dark:border-slate-600 dark:text-slate-200">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-      erat a ante.
-    </blockquote>
-  );
-}
+import { classed } from "~/utils/tw";
+
+export const TypographyBlockquote = classed.blockquote(
+  "mt-6 border-l-2 border-slate-300 pl-6 italic text-slate-800 dark:border-slate-600 dark:text-slate-200"
+);
