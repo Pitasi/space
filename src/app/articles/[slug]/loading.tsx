eb23fcb72d6baf35a6f462f312f2071674d88995
Antonio Pitasi <antonio@pitasi.dev>
2023-05-03 07:55:56 +0200

    add loading.tsx for articles

---
+++ src/app/articles/[slug]/loading.tsx
@@ -0,0 +1,20 @@
+import { Content } from "~/components/content";
+import { Header } from "~/components/header";
+import { Main } from "~/components/main";
+import { TypographyH2, TypographyP } from "~/components/ui/typography";
+
+export default function LoadingArticle() {
+  return (
+    <Main>
+      <Header title="Loading..." backHref="/articles" />
+
+      <Content title="Loading...">
+        <TypographyH2>Spinner vs skeletons</TypographyH2>
+        <TypographyP>
+          Instead of a spinner, I&apos;m writing this content to keep you busy
+          while my server is running an expensive database query.
+        </TypographyP>
+      </Content>
+    </Main>
+  );
+}
