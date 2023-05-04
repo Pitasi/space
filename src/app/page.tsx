d0139a888a3f4e1c9748632e8ce50823b3d4d6bb
Antonio Pitasi <antonio@pitasi.dev>
2023-05-04 14:13:48 +0200

    add homepage text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
+import Link from "next/link";
+import { TypographyP } from "~/components/ui/typography";
+
 const Page = () => {
-  return null;
+  return (
+    <main>
+      <TypographyP>
+        Hey there. I&apos;m Antonio, a software engineer based in Pisa, Italy. I
+        work with backend technologies and I&apos;m passionate about distributed
+        systems. In my free time, I organize events with the local community I
+        founded:{" "}
+        <Link
+          className="text-violet underline"
+          href="https://pisa.dev"
+          target="_blank"
+        >
+          pisa.dev
+        </Link>
+        .
+      </TypographyP>
+    </main>
+  );
 };
 
 export default Page;
