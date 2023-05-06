db5fa6b3fdb4b0700064cda7042ad54711d6f118
Antonio Pitasi <antonio@pitasi.dev>
2023-05-06 15:33:30 +0200

    fix code overflowing width

--- src/components/ui/typography/TypographyPre.tsx
+++ src/components/ui/typography/TypographyPre.tsx
@@ -2,5 +2,5 @@ import { classed } from "~/utils/tw";
 
 export const TypographyPre = classed(
   "pre",
-  "bg-white min-w-full [&>code]:w-full [&>code]:inline-block rounded-md [&>code]:bg-transparent"
+  "bg-white min-w-full overflow-x-auto [&>code]:w-full [&>code]:inline-block rounded-md [&>code]:bg-transparent"
 );
