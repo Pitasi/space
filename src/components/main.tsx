93e0db928e99de047d1ef3d82b5d39696dd023db
Antonio Pitasi <antonio@pitasi.dev>
2023-05-06 15:07:37 +0200

    fix sticky header on desktop

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,5 +1,3 @@
 export function Main({ children }: { children: React.ReactNode }) {
-  return (
-    <main className="relative pb-12 lg:overflow-auto lg:pb-0">{children}</main>
-  );
+  return <main className="relative pb-24 lg:pb-0">{children}</main>;
 }
