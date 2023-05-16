127a3b39c36055407c11ff2aa26186c8ac7abe02
Antonio Pitasi <antonio@pitasi.dev>
2023-05-16 12:10:45 +0200

    fix build

--- src/components/comments/comments.tsx
+++ src/components/comments/comments.tsx
@@ -3,17 +3,16 @@ import { AddCommentDialog } from "./dialog";
 import { addArticleComment } from "~/app/mutations/addComment";
 import { LoginGate } from "../login-gate";
 
-export async function Comments(props: {
+export function Comments(props: {
   id: string;
   kind: "article";
   children: React.ReactNode;
 }) {
-  const session = await getSessionRSC();
   return (
     <section className="bg-floralwhite">
       <div className="space-y-4 border-y-2 border-black bg-violet bg-pattern-plus px-8 py-24">
         <h1 className="font-neu text-6xl font-bold">Comments section</h1>
-        <LoginGate session={session}>
+        <LoginGate>
           <AddCommentDialog
             onSubmit={async ({ content }) => {
               "use server";
